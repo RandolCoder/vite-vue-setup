0b5f2bdd6a407528271eb28f4ca8d5978936bf5f
Randol <randol32@hotmail.com>
2022-08-28 16:11:46 -0500

    Configuración vue router

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,12 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <nav>
+    <router-link to="/">App</router-link> |
+    <router-link to="/home">Home</router-link>
+  </nav>
+  
+  <router-view></router-view>
 </template>
 
 <style scoped>
