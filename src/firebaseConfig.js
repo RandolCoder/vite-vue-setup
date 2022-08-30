2e869a1fb626273a14d8547bc39f819546ecb0bd
Randol <randol32@hotmail.com>
2022-08-30 11:44:19 -0500

    configuracion Firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDnGBbAjJ7MkP136IspFWOJHnng3PzoBc4",
+    authDomain: "vite-vue-setup.firebaseapp.com",
+    projectId: "vite-vue-setup",
+    storageBucket: "vite-vue-setup.appspot.com",
+    messagingSenderId: "640252701465",
+    appId: "1:640252701465:web:fd309f8d6f69d3afed7388"
+};
+
+initializeApp(firebaseConfig);
