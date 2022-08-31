e6c76817bb645081e0a5cf58ce5f550af445bbcd
Randol <randol32@hotmail.com>
2022-08-31 10:46:37 -0500

    Se Crea la vista Login y se configura como ruta de inicio. Para construir Login se utiliza block de prime vue

--- src/router.js
+++ src/router.js
@@ -1,7 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "./views/Home.vue";
+import Login from "./views/Login.vue";
 
 const routes = [
+    { path: "/", component: Login },
     { path: "/home", component: Home },
 ];
 
---
+++ src/views/Login.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="h-screen flex align-items-center justify-content-center">
+        <div class="surface-card p-4 shadow-2 border-round w-full lg:w-6">
+            <div class="text-center mb-5">
+                <img src="https://media.istockphoto.com/vectors/vector-handwritten-logo-letter-r-vector-id1008257372?k=20&m=1008257372&s=170667a&w=0&h=Jq9BJYhOCzEL2q7aWBHa9_rvCwlw4SUb_SlnK0mRQ40=" alt="Image" height="50" class="mb-3">
+                <div class="text-900 text-3xl font-medium mb-3">Welcome Back</div>
+                <span class="text-600 font-medium line-height-3">Don't have an account?</span>
+                <a class="font-medium no-underline ml-2 text-blue-500 cursor-pointer">Create today!</a>
+            </div>
+    
+            <div>
+                <label for="email1" class="block text-900 font-medium mb-2">Email</label>
+                <InputText id="email1" type="text" class="w-full mb-3" />
+        
+                <label for="password1" class="block text-900 font-medium mb-2">Password</label>
+                <InputText id="password1" type="password" class="w-full mb-3" />
+        
+                <Button label="Sign In" icon="pi pi-user" class="w-full"></Button>
+            </div>
+        </div>
+    </div>
+</template>
